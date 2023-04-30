dfcc0969cbd24e5d613165c3b22381262cf8d0e3
jun02160 <jun020216@naver.com>
2023-04-30 18:09:35 +0900

    [ch02] JPA 시작

--- CH02-JPA-start1/src/main/java/jjun/server/ch02jpastart1/Member.java
+++ CH02-JPA-start1/src/main/java/jjun/server/ch02jpastart1/Member.java
@@ -1,5 +1,39 @@
 package jjun.server.ch02jpastart1;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Entity
+@Table(name="MEMBER")
 public class Member {
+
+    private String id;
+
+    private String username;
+
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
