65a72036bbc29a22a785a6c389291dc9bff6c43a
jun02160 <jun020216@naver.com>
2023-05-08 19:49:43 +0900

    [ch10] 객체지향 쿼리 언어

---
+++ CH10-book/src/main/java/jjun/server/ch10book/Ch10BookApplication.java
@@ -0,0 +1,13 @@
+package jjun.server.ch10book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10BookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch10BookApplication.class, args);
+    }
+
+}
---
+++ CH10-book/src/test/java/jjun/server/ch10book/Ch10BookApplicationTests.java
@@ -0,0 +1,13 @@
+package jjun.server.ch10book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch10BookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
