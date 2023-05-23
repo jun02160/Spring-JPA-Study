bc8db8802bdd62a34124397954f642015d859515
jun02160 <jun020216@naver.com>
2023-05-23 10:33:44 +0900

    [ADD] 실전 스프링부트와 JPA 활용2 초기 세팅

---
+++ API-advanded/src/main/java/jjun/server/apiadvanded/ApiAdvandedApplication.java
@@ -0,0 +1,13 @@
+package jjun.server.apiadvanded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdvandedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAdvandedApplication.class, args);
+    }
+
+}
---
+++ API-advanded/src/test/java/jjun/server/apiadvanded/ApiAdvandedApplicationTests.java
@@ -0,0 +1,13 @@
+package jjun.server.apiadvanded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdvandedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
